8be8961242be52be9f88e0368a1b93aaf09896e3
Colby Hall <me@cobeh.com>
2019-06-23 18:32:45 -0700

    working on file stuff

--- win32/ch_filesystem_win32.cpp
+++ win32/ch_filesystem_win32.cpp
@@ -9,20 +9,81 @@
 #define NOMINMAX
 #include <windows.h>
 
-ch::OS_Stream ch::std_out = GetStdHandle(STD_OUTPUT_HANDLE);
-ch::OS_Stream ch::std_err = GetStdHandle(STD_ERROR_HANDLE);
+ch::Stream ch::std_out = GetStdHandle(STD_OUTPUT_HANDLE);
+ch::Stream ch::std_err = GetStdHandle(STD_ERROR_HANDLE);
 
-ch::OS_Stream& ch::OS_Stream::write_raw(const void* ptr, usize size) {
+ch::Stream& ch::Stream::write_raw(const void* ptr, usize size) {
 	WriteFile(os_handle, ptr, (DWORD)size, nullptr, nullptr);
 	return *this;
 }
 
-ch::OS_Stream& ch::OS_Stream::operator<<(const tchar* c_str) {
+ch::Stream& ch::Stream::operator<<(const tchar* c_str) {
 	return write_raw(c_str, ch::strlen(c_str));
 }
 
-ch::OS_Stream& ch::OS_Stream::operator<<(const tchar c) {
+ch::Stream& ch::Stream::operator<<(const tchar c) {
     return write_raw(&c, 1);
+}
+
+bool ch::File::open(const tchar* path, u32 open_flags) {
+    flags = open_flags;
+    const bool read = open_flags & FO_Read;
+    const bool write = open_flags & FO_Write;
+    const bool binary = open_flags & FO_Binary;
+    const bool lock = open_flags & FO_Lock;
+    const bool create = open_flags & FO_Create;
+
+    assert(read || write);
+    DWORD desired_access = 0;
+    if (read) desired_access |= GENERIC_READ;
+    if (write) desired_access |= GENERIC_WRITE;
+
+    DWORD creation = OPEN_EXISTING;
+    if (create) creation = CREATE_NEW;
+
+    os_handle = CreateFile(path, desired_access, 0, NULL, creation, FILE_ATTRIBUTE_NORMAL, NULL);
+    is_open = os_handle != INVALID_HANDLE_VALUE;
+    return is_open;
+}
+
+bool ch::File::close() {
+    if (is_open && CloseHandle(os_handle)) {
+        is_open = false;
+    }
+
+    return is_open;
+}
+
+void ch::File::read(void* dest, usize size) {
+    assert(is_open);
+    ReadFile(os_handle, dest, (DWORD)size, NULL, NULL);
+}
+
+void ch::File::seek_top() {
+    assert(is_open);
+    SetFilePointer(os_handle, NULL, NULL, FILE_BEGIN);
+}
+
+void ch::File::seek_bottom() {
+    assert(is_open);
+    SetFilePointer(os_handle, NULL, NULL, FILE_END);
+}
+
+void ch::File::seek(ssize amount) {
+    assert(is_open);
+    SetFilePointer(os_handle, NULL, (PLONG)amount, FILE_CURRENT);
+}
+
+void ch::File::set_end_of_file() {
+    assert(is_open);
+    SetEndOfFile(os_handle);
+}
+
+usize ch::File::size() const {
+    assert(is_open);
+    const DWORD file_size = GetFileSize(os_handle, NULL);
+
+    return (usize)file_size;
 }
 
 ch::String ch::get_current_path() {

6ccdd86b67b85d565d7c87edcb030263d360fd11
Colbeyyy <colbeyyyhall@gmail.com>
2019-08-19 22:38:22 -0700

    finished pool allocator

--- pool_allocator.cpp
+++ pool_allocator.cpp
@@ -1,70 +1,47 @@
 #include "pool_allocator.h"
 
-static void* pool_alloc(const ch::Allocator& allocator, usize size) {
-	ch::Pool_Allocator_Header* header = allocator.get_header<ch::Pool_Allocator_Header>();
-
-	ssize start_index = -1;
-	usize space_found = 0;
-	for (usize i = 0; i < header->buckets.count; i++) {
-		ch::Pool_Bucket& it = header->buckets[i];
-
-		if (!it.allocation && start_index == -1) {
-			start_index = i;
-		}
-		else if (it.allocation) {
-			start_index = -1;
-			space_found = 0;
-		}
-
-		if (start_index != -1) {
-			space_found += 1;
-		}
-
-		if ((space_found * header->bucket_size) >= size) {
-			break;
-		}
-	}
-
-	if (start_index == -1 || (space_found * header->bucket_size) < size) return nullptr;
-
-	void* allocation = (u8*)allocator.data + sizeof(*header) + (start_index * header->bucket_size);
-
-	for (usize i = 0; i < space_found; i++) {
-		ch::Pool_Bucket& it = header->buckets[i + start_index];
-
-		it.allocation = allocation;
-		it.used = size > header->bucket_size ? header->bucket_size : size;
-		size -= header->bucket_size;
-	}
-
-	return allocation;
-}
-
-static void pool_free(const ch::Allocator& allocator, void* ptr) {
-	ch::Pool_Allocator_Header* header = allocator.get_header<ch::Pool_Allocator_Header>();
-
-	// @NOTE(CHall): run through our buckets and reset them if they match out allocation passed in by ptr
-	bool found_allocation = false;
-	for (ch::Pool_Bucket& it : header->buckets) {
-		if (it.allocation == ptr) {
-			found_allocation = true;
-		}
-
-		if (found_allocation && it.allocation != ptr) return;
-
-		if (found_allocation) {
-			it.allocation = nullptr;
-			it.used = 0;
-		}
-	}
-}
-
-static void* pool_allocator_func(const ch::Allocator& allocator, void* ptr, usize size) {
+static void* pool_alloc(const ch::Allocator& allocator, void* ptr, usize size) {
 	ch::Pool_Allocator_Header* header = allocator.get_header<ch::Pool_Allocator_Header>();
 
 	if (size) {
 		if (!ptr) {
-			return pool_alloc(allocator, size);
+			ch::Pool_Allocator_Header* header = allocator.get_header<ch::Pool_Allocator_Header>();
+
+			ssize start_index = -1;
+			usize space_found = 0;
+			for (usize i = 0; i < header->buckets.count; i++) {
+				ch::Pool_Bucket& it = header->buckets[i];
+
+				if (!it.allocation && start_index == -1) {
+					start_index = i;
+				}
+				else if (it.allocation) {
+					start_index = -1;
+					space_found = 0;
+				}
+
+				if (start_index != -1) {
+					space_found += 1;
+				}
+
+				if ((space_found * header->bucket_size) >= size) {
+					break;
+				}
+			}
+
+			if (start_index == -1 || (space_found * header->bucket_size) < size) return nullptr;
+
+			void* allocation = (u8*)allocator.data + sizeof(*header) + (start_index * header->bucket_size);
+
+			for (usize i = 0; i < space_found; i++) {
+				ch::Pool_Bucket& it = header->buckets[i + start_index];
+
+				it.allocation = allocation;
+				it.used = size > header->bucket_size ? header->bucket_size : size;
+				size -= header->bucket_size;
+			}
+
+			return allocation;
 		} else {
 			usize old_size = 0;
 			bool found_allocation = false;
@@ -72,21 +49,45 @@ static void* pool_allocator_func(const ch::Allocator& allocator, void* ptr, usiz
 				ch::Pool_Bucket& it = header->buckets[i];
 
 				if (it.allocation == ptr) {
-					found_allocation = false;
+					found_allocation = true;
 				}
 
-				if (found_allocation && it.allocation != ptr) {
+				if (found_allocation && it.allocation && it.allocation != ptr) {
 					break;
 				}
 
-				// if (old_size + it.used > )
+				it.allocation = ptr;
+				it.used = header->bucket_size;
+
+				if (old_size + header->bucket_size >= size) {
+					it.used = size - old_size;
+					return it.allocation;
+				}
+
 				old_size += it.used;
 			}
 
-			// result = ch::realloc(ptr, size);
+			assert(old_size < size);
+
+			void* result = pool_alloc(allocator, nullptr, size);
+			ch::mem_copy(result, ptr, old_size);
+			pool_alloc(allocator, ptr, 0); // free
+			return result;
 		}
 	} else if (ptr) {
-		pool_free(allocator, ptr);
+		bool found_allocation = false;
+		for (ch::Pool_Bucket& it : header->buckets) {
+			if (it.allocation == ptr) {
+				found_allocation = true;
+			}
+
+			if (found_allocation && it.allocation != ptr) break;;
+
+			if (found_allocation) {
+				it.allocation = nullptr;
+				it.used = 0;
+			}
+		}
 	}
 
 	return nullptr;
@@ -112,7 +113,7 @@ ch::Allocator ch::make_pool_allocator(usize bucket_size, usize num_buckets) {
 
 	ch::Allocator result;
 	result.data = data;
-	result.func = pool_allocator_func;
+	result.func = pool_alloc;
 	return result;
 }
 
